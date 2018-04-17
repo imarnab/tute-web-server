2b63c5f74342e44728538aa6d833aa45f0bd4993
Arnab Chakraborty <imarnab123@gmail.com>
2018-04-17 13:14:12 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+// Middleware
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+            
+        }
+    });
+    next();
+});
+// Maintainance page middleware
+// app.use((req, res, next) => {
+//     res.render('maintainance.hbs');
+// });
+// Static pages
+app.use(express.static(__dirname + '/public'));
+// Handlebars helpers
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!!</h1>');
+    // res.send({
+    //     name: 'Arnab',
+    //     hobbies: [
+    //         'Photography',
+    //         'Travelling',
+    //         'Bike Riding',
+    //         'Reading'
+    //     ],
+    //     city: 'Kolkata'
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        content: 'This is home page content'
+    });
+});
+
+app.get('/about', (req, res) => {
+    // res.send('This is about page');
+    res.render('about.hbs', {
+        pageTitle: 'About Us'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Bad Request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on post 3000.');
+    
+});
